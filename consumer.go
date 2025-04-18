9ab2d23c5b22bc8168861b1c257f55c85c6d549b
Belyakov Sergey Sergeevich <sbsphp@gmail.com>
2025-04-18 16:34:52 +0700

    fix

--- consumer.go
+++ consumer.go
@@ -743,7 +743,11 @@ func (child *partitionConsumer) parseResponse(response *FetchResponse) ([]*Consu
 		} else if block.LastRecordsBatchOffset != nil && *block.LastRecordsBatchOffset < block.HighWaterMarkOffset {
 			// check last record offset to avoid stuck if high watermark was not reached
 			Logger.Printf("consumer/broker/%d received batch with zero records but high watermark was not reached, topic %s, partition %d, offset %d\n", child.broker.broker.ID(), child.topic, child.partition, *block.LastRecordsBatchOffset)
-			child.offset = *block.LastRecordsBatchOffset + 1
+			if child.offset == *block.LastRecordsBatchOffset+1 {
+				child.offset++
+			} else {
+				child.offset = *block.LastRecordsBatchOffset + 1
+			}
 		}
 
 		return nil, nil
